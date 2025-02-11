ec0e2deb93a254270fc7cc78695dea3dd664d914
Edwin14459 <santiagoedwin051@gmail.com>
2025-02-11 13:56:05 -0600

    Segundo Ejercicio

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+nombre=(input("Ingrese su nombre completo: "))
+carrera=(input("Ingrese su carrera cursante: "))
+semestre=(input("Ingrese el semestre en curso: "))
+materia=(input("Ingrese el nombre de la materia: "))
+
+print(f"Nombre Completo: {nombre}, Nombre Completo: {carrera}, Nombre Completo: {semestre}, Nombre Completo: {materia}")
