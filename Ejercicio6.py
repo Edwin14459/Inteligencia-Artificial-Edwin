beeacbcad4b042b612edcaf2696593e792b50dc4
Edwin14459 <santiagoedwin051@gmail.com>
2025-02-11 15:45:30 -0600

    Sexto Ejercicio

---
+++ Ejercicio6.py
@@ -0,0 +1,9 @@
+num1= 7.0
+num2= 7.0
+
+resultado_resta = num1 - num2
+resultado_multiplicacion = num1 * num2
+
+print("El resultado de la resta de 7.0 - 7.0 es:", resultado_resta)
+print("\nEl resultado de la multiplicación de 7.0 * 7.0 es:", resultado_multiplicacion)
+print("\n¡Programa realizado por Edwin!")
