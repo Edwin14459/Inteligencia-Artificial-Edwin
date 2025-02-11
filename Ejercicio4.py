8a49b24e46269cf7ae8c965e59c0b5741e989876
Edwin14459 <santiagoedwin051@gmail.com>
2025-02-11 14:14:15 -0600

    Cuarto Ejercicio

---
+++ Ejercicio4.py
@@ -0,0 +1,10 @@
+import math
+
+#Pedir el radio del circulo
+radio = float(input("Introduce el radio del circulo en metros:\n"))
+
+#Calcular en area de circulo (A = pi r ala 2)
+area = math.pi * (radio ** 2)
+
+#Mostrar el Resultado
+print(f"\nEl area del circulo con radio: {radio} metros es: {area:.2f} metros cuadrados")
