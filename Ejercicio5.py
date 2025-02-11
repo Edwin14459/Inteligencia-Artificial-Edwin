18e73675bf0091f15c7f309506f56d2bf521fcdd
Edwin14459 <santiagoedwin051@gmail.com>
2025-02-11 15:34:22 -0600

    Quinto Ejercicio

---
+++ Ejercicio5.py
@@ -0,0 +1,11 @@
+# Pedir el primer número  
+numero1 = float(input("Introduce el primer número: \n"))
+
+# Pedir el segundo número  
+numero2 = float(input("Introduce el segundo número: \n"))
+
+# Realizar una operación simple (suma)  
+suma = numero1 + numero2
+
+# Mostrar el resultado con salto de línea  
+print(f"La suma de {numero1} y {numero2} es: {suma}")
